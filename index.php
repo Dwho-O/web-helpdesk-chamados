cc04b02ff3b5850d28a6ab57bf4dcf9c72361b08
JoaoPaulo <dwho.disney@gmail.com>
2022-07-31 18:53:09 -0300

    criação da estrutura html

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <meta description="Site para abertura de chamados Helpdesk"/>
+        <link href="estilos/estilo.css" rel="stylesheet">
+        <script type="text/javascript" src="js/meu-arquivo.js"></script>
+    </head>
+    <body>
+        <nav class="navbar">
+            <li class="nav-indice" id="li-inicio"><a href="#">Início</a></li>
+            <li class="nav-indice" id="li-entrar"><a href="#">Entrar</a></li>
+            <li class="nav-indice" id="li-registrar"><a href="#">Registrar-se</a></li>
+        </nav>
+        
+    </body>
+    <footer>
+        
+    </footer>
+</html>
