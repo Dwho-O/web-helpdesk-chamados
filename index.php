192fd285bc6d1b809a204efea67be1ef6734452c
JoaoPaulo <dwho.disney@gmail.com>
2022-08-04 13:59:24 -0300

    melhorias e correções semânticas

--- index.php
+++ index.php
@@ -7,29 +7,36 @@
         <script type="text/javascript" src="js/meu-arquivo.js"></script>
     </head>
     <body>
-        <header>
-            <<img src="" alt="Imagem da logo do site, ela tem uma borda em forma de um círculo de cor preta e dentro do círculo tem uma letra 'H' preta que simboliza H de Helpdesk">
+        <!-- logo do site -->
+        <header class="logo-site">
+            <img src="" alt="Imagem da logo do site, ela tem uma borda em forma de um círculo de cor preta e dentro do círculo tem uma letra 'H' preta que simboliza H de Helpdesk">
         </header>
-        <nav class="navbar">
+
+        <!-- menu com os elementos da página -->
+        <nav class="navbar-menu">
             <li class="nav-indice" id="li-inicio"><a href="#">Início</a></li>
             <li class="nav-indice" id="li-entrar"><a href="#">Entrar</a></li>
             <li class="nav-indice" id="li-registrar"><a href="#">Registrar-se</a></li>
         </nav>
+
+        <!-- corpo do site principal -->
         <main class="estrutura-principal">
+            <!-- estrutura de artigos -->
             <article class="estrutura-postagem">
+                <!-- informações da postagem -->
                 <section class="conteudo-postagem">
 
                 </section>
+                <!-- menu lateral destinado a postagens recentes -->
                 <aside class="menu-lateral">
 
                 </aside>
             </article>
         </main>
+
+        <!-- links úteis no final da página -->
         <footer class="menu-inferior">
             
         </footer>
     </body>
-    <footer>
-        
-    </footer>
 </html>
