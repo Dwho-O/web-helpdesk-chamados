ac33c9f6d14942bea3bf2916f18900a72b6ee107
JoaoPaulo <dwho.disney@gmail.com>
2022-08-04 14:02:44 -0300

    melhorias nas tags meta do head

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8"/>
-        <meta description="Site para abertura de chamados Helpdesk"/>
+        <meta charset="UTF-8">
+        <meta name="description" content="Site para abertura de chamados Helpdesk">
+        <meta name="keywords" content="chamados, helpdesk, suporte técnico">
+        <meta name="author" content="João Paulo">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="estilos/estilo.css" rel="stylesheet">
         <script type="text/javascript" src="js/meu-arquivo.js"></script>
     </head>
